548eb5d2e5f3a6acf1ce5c89f434af7daf91ce01
Pamera-png <pameranatali@gmail.com>
2023-08-20 18:27:29 -0300

    adicionado dependencias e scripts sql criados

--- WorkSpaceCursoSpringBoot/minhas_vendas/src/main/java/com/rozembra/pamera/VendasApplication.java
+++ WorkSpaceCursoSpringBoot/minhas_vendas/src/main/java/com/rozembra/pamera/VendasApplication.java
@@ -1,15 +1,10 @@
 package com.rozembra.pamera;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 
 public class VendasApplication {
 
